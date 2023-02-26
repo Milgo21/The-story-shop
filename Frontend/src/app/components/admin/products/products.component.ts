6e8bb998fd47854fdade1c61835204d229679f7d
Milgo21 <felixmilgo21@gmail.com>
2023-02-26 18:54:04 +0300

    updatem

--- Frontend/src/app/components/admin/products/products.component.ts
+++ Frontend/src/app/components/admin/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Book } from 'src/app/interfaces/interfaces';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-products',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
+Books:Book[]=[]
+constructor(private service:ProductService){
 
+  this.Books = this.service.Books
+}
 }

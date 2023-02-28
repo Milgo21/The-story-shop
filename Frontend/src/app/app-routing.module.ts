f4390617e29b2d302ae63e5c1c87b95a66e7c6a5
CynthiaGichuki <gichukicynthia1@gmail.com>
2023-02-28 17:05:16 +0300

    Added Login Guard and Service

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
   // {path:'', component: HomepageComponent},
-  {path:'', loadComponent: () => import('./components/homepage/homepage.component').then(c => c.HomepageComponent)},
-  {path:'login', loadComponent: () => import('./components/login/login.component').then(c => c.LoginComponent)},
-  {path:'register', loadComponent: () => import('./components/registration/registration.component').then(c => c.RegistrationComponent)},
-  {path:'products', loadComponent: () => import('./components/displayproducts/displayproducts.component').then(c => c.DisplayproductsComponent)},
-  {path:'admin', loadComponent: () => import('./components/admin/admin.component').then(c => c.AdminComponent),
-     children: [
-      {path: 'products', loadComponent: () => import('./components/admin/products/products.component').then(c => c.ProductsComponent)},
-      {path: 'orders', loadComponent: () => import('./components/admin/orders/orders.component').then(c => c.OrdersComponent)},
-      {path: 'customers', loadComponent: () => import('./components/admin/customers/customers.component').then(c => c.CustomersComponent)},
-      {path: 'add-product', loadComponent: () => import('./components/admin/addproduct/addproduct.component').then(c => c.AddproductComponent)}
-     ]
+  { path: '', loadComponent: () => import('./components/homepage/homepage.component').then(c => c.HomepageComponent) },
+  { path: 'login', loadComponent: () => import('./components/login/login.component').then(c => c.LoginComponent) },
+  { path: 'register', loadComponent: () => import('./components/registration/registration.component').then(c => c.RegistrationComponent) },
+  { path: 'products', canActivate: [AuthGuardService], loadComponent: () => import('./components/displayproducts/displayproducts.component').then(c => c.DisplayproductsComponent) },
+  {
+    path: 'admin', loadComponent: () => import('./components/admin/admin.component').then(c => c.AdminComponent),
+    children: [
+      { path: 'products', loadComponent: () => import('./components/admin/products/products.component').then(c => c.ProductsComponent) },
+      { path: 'orders', loadComponent: () => import('./components/admin/orders/orders.component').then(c => c.OrdersComponent) },
+      { path: 'customers', loadComponent: () => import('./components/admin/customers/customers.component').then(c => c.CustomersComponent) },
+      { path: 'add-product', loadComponent: () => import('./components/admin/addproduct/addproduct.component').then(c => c.AddproductComponent) }
+    ]
   },
 
 
